c6b283bfe57900dac0fefd6e11bfcda1b1468395
Omer Ugur Kilavuz <omerugurkilavuz@gmail.com>
2026-05-16 23:51:12 +0300

    chore: export AwardCard, TabBar, ParticleBurst from components index

--- components/index.ts
+++ components/index.ts
@@ -4,8 +4,6 @@ export { AuroraBackground } from './AuroraBackground';
 export { NotifToggle } from './NotifToggle';
 export { PillBadge } from './PillBadge';
 export { FilterChips } from './FilterChips';
-// Placeholder (henüz implement edilmeyecek, sonraki adımda):
-// export { AwardCard } from './AwardCard';
-// export { BentoGrid } from './BentoGrid';
-// export { UrgentHero } from './UrgentHero';
-// export { ArticleCard } from './ArticleCard';
+export { default as AwardCard } from './AwardCard';
+export { default as TabBar } from './TabBar';
+export { default as ParticleBurst } from './ParticleBurst';
